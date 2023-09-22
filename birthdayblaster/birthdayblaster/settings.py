69f856806612f522a5aaa453e25c68fd8e507efc
daceyforward <dacey.loving@gmail.com>
2023-09-22 14:44:34 -0400

    deployment trial 2

--- birthdayblaster/birthdayblaster/settings.py
+++ birthdayblaster/birthdayblaster/settings.py
@@ -24,8 +24,7 @@ env = environ.Env(
 # environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
-# .parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
